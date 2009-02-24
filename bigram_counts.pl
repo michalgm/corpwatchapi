b66ee22f1f78e4acd55df72430ad7aaa7abc9d30
Skye Bender-deMoll <skyebend@skyeome.net>
2009-02-24 08:06:36 +0000

    tweaked bigram, created bigram freq table
    git-svn-id: file:///var/lib/svn/edgarapi@63 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- bigram_counts.pl
+++ bigram_counts.pl
@@ -7,7 +7,7 @@ my $max_results = 0; #set this to 0 if you want all results
 our $db;
 my %dict;
 
-my $names = $db->selectall_arrayref('select edgar_name from cik_name_lookup');
+my $names = $db->selectall_arrayref('select name, source from company_names where source != "filer_conformed_name" group by name');
 foreach my $name (@$names) {
 	$name = $name->[0];
 	my @words = split(/[\s\/]+/, $name);
@@ -21,7 +21,7 @@ foreach my $name (@$names) {
 my @word_list = sort { $dict{$b} <=> $dict{$a} } keys(%dict);
 
 unless ($max_results) { $max_results = $#word_list; }
-foreach my $x (0 .. 500) {
+foreach my $x (0 .. $max_results) {
 	print "$word_list[$x]\t$dict{$word_list[$x]}\n";
 }
 
