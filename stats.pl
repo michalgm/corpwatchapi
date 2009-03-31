1d410e6a215fc8f879853de9f032e821a0debe47
Greg Michalec <greg@primate.net>
2009-03-31 22:23:09 +0000

    added a few more stats, cleaned things up
    git-svn-id: file:///var/lib/svn/edgarapi@111 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- stats.pl
+++ stats.pl
@@ -1,16 +1,25 @@
 #!/usr/bin/perl 
 use CGI;
 print CGI::header();
-#chdir("/home/dameat/edgarapi/backend/"); 
-#require "common.pl";
-my $db = dbconnect;
-my $query = "
-
-select count(*)  from filings where has_sec21 =1 union select  count(distinct filing_id) from relationships union select num2/num1*100 from (select count(*) as num1 from filings a where has_sec21 =1) a  join (select  count(distinct filing_id) as num2 from relationships) b union select count(*) from relationships union select concat(num1/num2*100, ' ', num2-num1) from (select count(*) as num1 from relationships a where country_code is not null) a  join (select  count(*) as num2 from relationships) b
-
-";
+chdir("/home/dameat/edgarapi/backend/"); 
+require "./common.pl";
+my $db = &dbconnect();
+my @queries = ( 
+"select 'filings w/ sec21', count(*)  from filings where has_sec21 =1",
+"select 'filings with relationships parsed', count(distinct filing_id) from relationships",
+"select 'percentage of filings parsed', num2/num1*100 from (select count(*) as num1 from filings a where has_sec21 =1) a  join (select  count(distinct filing_id) as num2 from relationships) b",
+"select 'filings with empty sec21', count(*) from filings a join no_relationship_filings using(filing_id) where code = 1 ",
+"select 'percentage of true filings parsed', num2/(num1-num3)*100 from (select count(*) as num1 from filings a where has_sec21 =1) a  join (select  count(distinct filing_id) as num2 from relationships) b join (select count(*) as num3 from filings a join no_relationship_filings using(filing_id) where code = 1) c",
+"select 'number of relationships', count(*) from relationships",
+"select 'percentage of relationships w/ location', concat(num1/num2*100, ' ', num2-num1) from (select count(*) as num1 from relationships a where country_code is not null) a  join (select  count(*) as num2 from relationships) b",
+"select 'number of companies', count(*) from companies",
+"select 'number of company relationships', count(*) from company_relations",
+"select 'companies w/0 relationships', count(distinct cw_id) from companies a where num_children =0 and num_parents =0",
+"select 'top-level companies', count(distinct cw_id) from companies a left join company_relations b on cw_id = target_cw_id where relation_id is null",
+"select 'top-level companies with children', count(distinct cw_id) from companies a join company_relations c on cw_id = c.source_cw_id left join company_relations b on cw_id = b.target_cw_id where b.relation_id is null");
 print "<table>";
-foreach my $row (@{$db->selectall_arrayref($query)}) {
+foreach my $query (@queries) {
+	$row = $db->selectrow_arrayref($query);
 	print "<tr>";
 	foreach my $cell (@$row) {
 		print "<td>$cell</td>";
@@ -20,19 +29,4 @@ foreach my $row (@{$db->selectall_arrayref($query)}) {
 
 
 print "</table>";
-sub dbconnect {
-	my $db = shift;
-	unless($db) { $db = 'edgarapi';}
-	my $dsn = "dbi:mysql:$db:localhost;mysql_compression=1";
-	my $dbh;
-	while (!$dbh) {
-		$dbh = DBI->connect($dsn, 'edgar', 'edgar', {'mysql_enable_utf8'=>1});
-		unless ($dbh) {
-			print("Unable to Connect to database\n");
-			sleep(10);
-		}
-	}
-	$dbh->{'mysql_auto_reconnect'} = 1;
-	return $dbh;
-}
 
