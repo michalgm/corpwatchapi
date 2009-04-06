eff29acad5f75f49228c2c55967aceb04e2beac4
Greg Michalec <greg@primate.net>
2009-04-06 08:04:01 +0000

    remove limit 1000 t actually update all the filing urls
    git-svn-id: file:///var/lib/svn/edgarapi@128 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- update_sec21_urls.pl
+++ update_sec21_urls.pl
@@ -5,7 +5,7 @@ require "common.pl";
 our $db;
 our $datadir;
 	my $sth = $db->prepare("update filings set sec_21_url = ? where filing_id = ?");
-	$filing = $db->selectall_arrayref("select filing_id, filename, quarter, year, a.cik, company_name from filings a $join where has_sec21 = 1 $where order by company_name limit 1000") || die "$!";
+	$filing = $db->selectall_arrayref("select filing_id, filename, quarter, year, a.cik, company_name from filings a $join where has_sec21 = 1 $where order by company_name") || die "$!";
 	foreach my $filing (@$filing) {
 		open(FILE, "$datadir/$filing->[3]/$filing->[2]/$filing->[0].sec21");
 		my $filename;
