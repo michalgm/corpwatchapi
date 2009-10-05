f8c5381d6661e8452b13ff5b5b6daaca5c450f2d
Greg Michalec <greg@primate.net>
2009-10-05 23:27:13 +0000

    added GPL and description to fetch_filer_headers.pl
    git-svn-id: file:///var/lib/svn/edgarapi@254 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- fetch_filer_headers.pl
+++ fetch_filer_headers.pl
@@ -1,5 +1,29 @@
 #!/usr/bin/perl -w
 
+    # Copyright 2009 CorpWatch.org 
+    # San Francisco, CA | 94110, USA | Tel: +1-415-641-1633
+    # Developed by Greg Michalec and Skye Bender-deMoll
+    
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+#---------------------------------------
+# This script fetches html header files for filings, to be parsed by parse_headers.pl
+#---------------------------------------
+
+
+
+
 #use this if yo uneed to: grep -rl "Sorry, there was a problem" data/* | xargs rm
 
 require './common.pl';
