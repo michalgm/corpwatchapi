56e0a902bede20cace4f9d68b60df5598902cacf
Greg Michalec <greg@primate.net>
2009-10-05 23:36:16 +0000

    stats.pl: 	* group by year
    git-svn-id: file:///var/lib/svn/edgarapi@257 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- stats.pl
+++ stats.pl
@@ -26,21 +26,26 @@ print CGI::header();
 chdir "/home/dameat/edgarapi/backend/"; #This should be set to the full path of this script
 require "./common.pl";
 my $db = &dbconnect();
+
+foreach my $year (@{$db->selectall_arrayref("select year from filings group by year")}) {
+	$year = $year->[0];
+print "<h3>$year</h3>";
+
 my @queries = ( 
-"select 'filing companies', count(*) from filers",
-"select 'filings w/ sec21', count(*)  from filings where has_sec21 =1",
-"select 'filings with relationships parsed', count(distinct filing_id) from relationships",
-"select 'percentage of filings parsed', num2/num1*100 from (select count(*) as num1 from filings a where has_sec21 =1) a  join (select  count(distinct filing_id) as num2 from relationships) b",
-"select 'filings with empty sec21', count(*) from filings a join no_relationship_filings using(filing_id) where code = 1 ",
-"select 'percentage of true filings parsed', num2/(num1-num3)*100 from (select count(*) as num1 from filings a where has_sec21 =1) a  join (select  count(distinct filing_id) as num2 from relationships) b join (select count(*) as num3 from filings a join no_relationship_filings using(filing_id) where code = 1) c",
-"select 'number of relationships', count(*) from relationships",
-"select 'percentage of relationships w/ location', concat(num1/num2*100, ' ', num2-num1) from (select count(*) as num1 from relationships a where country_code is not null) a  join (select  count(*) as num2 from relationships) b",
-"select 'number of companies', count(*) from companies",
-"select 'number of company relationships', count(*) from company_relations",
-"select 'companies w/o parents or children' , count(distinct cw_id) from companies a where num_children =0 and num_parents =0",
-"select 'top-level companies', count(distinct cw_id) from companies a left join company_relations b on cw_id = target_cw_id where relation_id is null",
-"select 'top-level companies with children', count(distinct cw_id) from companies a join company_relations c on cw_id = c.source_cw_id left join company_relations b on cw_id = b.target_cw_id where b.relation_id is null",
-"select 'filers with hierarchy', count(*) from (SELECT c.cw_id FROM relationships a join filings b using (filing_id)  join companies c on b.cik = c.cik where parent_cw_id != c.cw_id group by c.cw_id) a");
+"select 'filing companies', count(*) from filers where year = $year",
+"select 'filings w/ sec21', count(*)  from filings where has_sec21 =1 and year = $year",
+"select 'filings with relationships parsed', count(distinct filing_id) from relationships join filings using(filing_id) where filings.year = $year",
+"select 'percentage of filings parsed', num2/num1*100 from (select count(*) as num1 from filings a where has_sec21 =1  and year = $year) a  join (select  count(distinct filing_id) as num2 from relationships  join filings using(filing_id) where filings.year = $year) b",
+"select 'filings with empty sec21', count(*) from filings a join no_relationship_filings using(filing_id) where code = 1 and year = $year",
+"select 'percentage of true filings parsed', num2/(num1-num3)*100 from (select count(*) as num1 from filings a where has_sec21 =1 and year = $year) a  join (select  count(distinct filing_id) as num2 from relationships join filings using(filing_id) where filings.year = $year) b join (select count(*) as num3 from filings a join no_relationship_filings using(filing_id) where code = 1  and a.year = $year) c",
+"select 'number of relationships', count(*) from relationships where year = $year",
+#"select 'percentage of relationships w/ location', concat(num1/num2*100, ' ', num2-num1) from (select count(*) as num1 from relationships a where country_code is not null) a  join (select  count(*) as num2 from relationships) b",
+"select 'number of companies', count(*) from company_info where year = $year",
+"select 'number of company relationships', count(*) from company_relations where year = $year",
+"select 'companies w/o parents or children' , count(distinct cw_id) from company_info a where num_children =0 and num_parents =0 and year = $year",
+"select 'top-level companies', count(distinct cw_id) from company_info a left join company_relations b on cw_id = target_cw_id and a.year = b.year where relation_id is null a and a.$year",
+"select 'top-level companies with children', count(distinct cw_id) from company_info a join company_relations c on cw_id = c.source_cw_id and a.year = c.year left join company_relations b on cw_id = b.target_cw_id where b.relation_id is null and a.year = $year",
+"select 'filers with hierarchy', count(*) from (SELECT c.cw_id FROM relationships a join filings b using (filing_id)  join company_info c on b.cik = c.cik and b.year = c.year where parent_cw_id != c.cw_id  and c.year = $year group by c.cw_id) a");
 print "<table>";
 foreach my $query (@queries) {
 	$row = $db->selectrow_arrayref($query);
@@ -53,4 +58,4 @@ foreach my $query (@queries) {
 
 
 print "</table>";
-
+}
