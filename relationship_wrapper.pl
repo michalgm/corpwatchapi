631f9a9fea534c5df1d5c94f7cea3f8d0f9fc7c6
Greg Michalec <greg@primate.net>
2009-10-05 23:38:05 +0000

    relationship_wrapper.pl: 	* accept years as arguments to only process single year 	* better forking
    git-svn-id: file:///var/lib/svn/edgarapi@258 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- relationship_wrapper.pl
+++ relationship_wrapper.pl
@@ -27,11 +27,17 @@ use Parallel::ForkManager;
 require "./common.pl";
 #$| =1;
 our $db;
+$year = $ARGV[0];
+if ($year ) {
+	$where = " and year='$year' ";
+} else {
+	$year = "year"; 
+}
 my $relationship_table = 'relationships';
 open (LOG, ">log.txt");
 print "deleting tables...\n";
-$db->do("delete from $relationship_table");
-$db->do("delete from filing_tables");
+$db->do("delete from filing_tables where filing_id in(select filing_id from  filings where year = $year)");
+$db->do("delete from $relationship_table  where filing_id in(select filing_id from  filings where year = $year)");
 $db->do("delete from bad_locations");
 $db->do("alter table $relationship_table auto_increment = 0");
 #$db->do("alter table $relationship_table disable keys");
@@ -40,9 +46,9 @@ $db->do("alter table filing_tables auto_increment = 0");
 $db->do("alter table bad_locations auto_increment = 0");
 print "done\n";
 
-my $filings = $db->selectall_arrayref("select filing_id, filename, quarter, year, cik, company_name from filings where has_sec21 = 1 order by filing_id") || die "$!";
+my $filings = $db->selectall_arrayref("select filing_id, filename, quarter, year, cik, company_name from filings where has_sec21 = 1 and year = $year order by filing_id") || die "$!";
 
-my $manager = new Parallel::ForkManager( 60 );
+my $manager = new Parallel::ForkManager( 5 );
 my ($x, $y) = 0;
 my $limit = int($#${filings}*.01);
 print "$#${filings} - $limit\n";
@@ -62,7 +68,8 @@ foreach my $filing (@$filings) {
 	$manager->finish;
 }
 
-#$manager->wait_all_children;
+$manager->wait_all_children;
+print "\nDone\n";
 #$db->do("alter table $relationship_table enable keys");
 #$db->do("alter table filing_tables enable keys");
 
