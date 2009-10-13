5ebb7b74c8cf774a208f718f57356747ecb400f3
Greg Michalec <greg@primate.net>
2009-10-13 01:53:28 +0000

    only update null fields unless given argument
    git-svn-id: file:///var/lib/svn/edgarapi@279 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- update_sec21_urls.pl
+++ update_sec21_urls.pl
@@ -25,21 +25,26 @@ require "common.pl";
 
 our $db;
 our $datadir;
-	my $sth = $db->prepare("update filings set sec_21_url = ? where filing_id = ?");
-	$filing = $db->selectall_arrayref("select filing_id, filename, quarter, year, a.cik, company_name from filings a $join where has_sec21 = 1 $where order by company_name") || die "$!";
-	foreach my $filing (@$filing) {
-		open(FILE, "$datadir/$filing->[3]/$filing->[2]/$filing->[0].sec21");
-		my $filename;
-		while (<FILE>) { 
-			if ($_ =~ /^<FILENAME>(.+)/) {
-				$filename = $1;
-				last;
-			}
+
+unless ($ARGV[0]) { 
+	$where = " and sec_21_url is null ";
+}
+
+my $sth = $db->prepare("update filings set sec_21_url = ? where filing_id = ?");
+$filing = $db->selectall_arrayref("select filing_id, filename, quarter, year, a.cik, company_name from filings a $join where has_sec21 = 1 $where order by company_name") || die "$!";
+foreach my $filing (@$filing) {
+	open(FILE, "$datadir/$filing->[3]/$filing->[2]/$filing->[0].sec21");
+	my $filename;
+	while (<FILE>) { 
+		if ($_ =~ /^<FILENAME>(.+)/) {
+			$filename = $1;
+			last;
 		}
-		my $path = $filing->[1];
-		$path =~ s/\-//g;
-		$path =~ s/.{4}$//;
-		$filename = "http://www.sec.gov/Archives/$path/$filename";
-		$sth->execute($filename, $filing->[0]);
 	}
+	my $path = $filing->[1];
+	$path =~ s/\-//g;
+	$path =~ s/.{4}$//;
+	$filename = "http://www.sec.gov/Archives/$path/$filename";
+	$sth->execute($filename, $filing->[0]);
+}
 
