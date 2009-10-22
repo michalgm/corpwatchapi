b7f4672da1a16c9790314f9ba58d2b96020209ed
Greg Michalec <greg@primate.net>
2009-10-22 10:46:57 +0000

    cleaner output standard arguments more efficient
    git-svn-id: file:///var/lib/svn/edgarapi@298 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- parse_headers.pl
+++ parse_headers.pl
@@ -27,106 +27,111 @@ use HTML::TreeBuilder;
 use Data::Dumper;
 use Parallel::ForkManager;
 require './common.pl';
+our $db;
+$| = 1;
 
-my $doyear = $ARGV[0];
-my $filing_id = $ARGV[1];
-
-if ($doyear) {
-	unless ($filing_id) { 
-		$db->do("delete from filers where year = $doyear");
-	}
-	@years = ($doyear);
-} else { 
-	$db->do("delete from filers");
-	$db->do("alter table filers auto_increment= 0");
-	@years = (2003 .. 2009);
+my ($year, $nuke) = @ARGV;
+$where = "";
+if ($year) {
+	$where = " and year = $year ";
+	@years = ($year);
+} else {
+	@years = our @years_available;
 }
 
+print "Resetting...\n";
+if ($nuke) {
+	$db->do("delete from filers where filer_id = filer_id $where");
+	$db->do("alter table filers auto_increment= 0");
+}
+print "Caching already_parsed...\n";
 #my $file = "data/2008/1/68065.hdr"; 
 #my $cik = "0001156491";
 $db->disconnect;
-$manager = new Parallel::ForkManager( 5 );
-$manager->run_on_finish(
-	sub { 
-		my ($pid, $code, $ident) = @_; 
-		if ($code == 20) {
-			$manager->wait_all_children;
-			#exit;
-		}
-	}
-);
 
-if ($filing_id) { 
-	chomp($filing_id);
-	*LOG = *STDOUT; 
-} else {
-	open (LOG, '>parse_headers.log');
-}
+our $logdir;
+open (LOG, ">$logdir/parse_headers.log");
+my $manager = new Parallel::ForkManager( 5 );
+
 foreach my $year (@years) {
+	my $already_parsed = $db->selectall_hashref("select b.filing_id from filings b where b.year = $year", 'filing_id');
 	foreach my $q (1 .. 4) {
+		my @check_headers;
 		my $dir = "$datadir/$year/$q/" || die("can't open $dir");
 		opendir(DIR, $dir);
 		my @files = readdir(DIR);
 		closedir(DIR);
-		my $total = $#files;
-		my $set_size = int($total * .01);
-		$limit = int($total / $set_size * .01);
-		my ($x, $y) = 0;
-		print "\nParsing $year Q $q\n";
-		for my $set (0 .. int($total/$set_size)+1) { 
-			if ($x == $limit) { $y++; print "\r\t".($y)."%  "; $x = 0; } $x++;
-			my $pid = $manager->start and next;
-			$db = &dbconnect();
-			for my $count (1 .. $set_size) {
-				$count += ($set * $set_size);
-				if ($count > $total) { $manager->finish; }
-				my $file = @files[$count];
-				if ($file =~ /^(\d+)\.hdr$/) {
-					my $id = $1;
-					unless ($filing_id && $id != $filing_id) {
-						unless (my $filers = &parse_filers("$dir/$file", $id)) {
-							print LOG "$id didn't parse\n";
-						}
-						if ($filing_id) { 
-							$filing_id = 20;
-							last;
-						}
-					}
+		print "\nChecking $year Q $q\n";
+		foreach my $file (@files) {
+			if ($file =~ /^(\d+)\.hdr$/) {
+				my $id = $1;
+				unless ($already_parsed->{$id}) {
+					push(@check_headers, {id=>$id, file=>$file});
 				}
 			}
 			#if ($filing_id eq 'imdone') { print 'BAILING!!!!'; $manager->wait_all_children; exit; }
-			$manager->finish($filing_id);
-			print "\n";
+		}
+		my $total = $#check_headers;
+		if ($total > 0) {
+			print "\tNeed to parse $total headers\n";
+			my $set_size = int($total * .01);
+			if ($set_size == 0) { $set_size =1; }
+			$limit = int($total / $set_size * .01);
+			my ($x, $y) = 0;
+			for my $set (0 .. int($total/$set_size)+1) { 
+				if ($x == $limit) { $y++; print "\r\t".($y)."%"; $x = 0; } $x++;
+				my $pid = $manager->start and next;
+				$db = &dbconnect();
+				for my $count (1 .. $set_size) {
+					$count += ($set * $set_size);
+					if ($count > $total) { $manager->finish; }
+					my $id = $check_headers[$count]{id};
+					my $file = $check_headers[$count]{file};
+					unless (my $filers = &parse_filers("$dir/$file", $id)) {
+						print LOG "$id didn't parse\n";
+					}
+				}
+				$db->disconnect;
+				$manager->finish();
+			}
 		}
 	}
 }
 
+
 $manager->wait_all_children;
-print "\nUpdating filer years...\n";
-$db->do("update filers a join filings b using (filing_id) set a.year = b.year where a.year is null");
 print "\nAll DONE!";
 
 sub parse_filers() {
 	my ($file, $id) = @_;
-	#print "$file, $id";
 	my $filers;
-	my $sth = $db->prepare_cached('select cik from filings where filing_id = ?');
+	my $sth = $db->prepare_cached('select cik, year from filings where filing_id = ?');
 	$sth->execute($id);
 	my $res = $sth->fetchrow_arrayref();
 	unless ($res) { 
 		print "BaD: $id, $file\n"; 
+		`rm $file`;
 		return;
 	}
 
 	my $cik = $res->[0];
+	my $year = $res->[1];
 	$sth->finish;
 	#print "$file - $id - $cik\n";
 	my $tree = HTML::TreeBuilder->new_from_file($file);
+	unless ($tree->as_text =~ /$cik/) {
+		#If the filing doesn't have the cik in it, it's bogus - we'll delete it and get the update on the next fetch
+		$db->do("delete from filings where filing_id = $id");
+		`rm $file`;
+	}
 	my @filerdivs =  $tree->find_by_attribute('id', 'filerDiv');
 	foreach my $filerdiv (@filerdivs) {
-		unless ($filerdiv->as_text =~ /$cik/) { next; }
+		unless ($filerdiv->as_text =~ /$cik/) { 
+			#This is not the div you're looking for
+			next; 
+		}
 	#	print "##\n\n".$filerdiv->as_HTML."\n";
-		my $values = {'filing_id'=>$id, 'cik'=>$cik};
+		my $values = {'filing_id'=>$id, 'cik'=>$cik, 'year'=>$year};
 		my @results;
 		foreach my $address ($filerdiv->find_by_attribute('class', 'mailer')) {
 			$type = $address->as_text =~ /^Business/ ? 'business' : 'mail';
@@ -188,9 +193,9 @@ sub parse_filers() {
 		}
 		
 		push(@$filers, $values);
-		my $sth2 = $db->prepare_cached("insert into filers (filer_id, filing_id, cik, irs_number, conformed_name, fiscal_year_end, sic_code, business_street_1, business_street_2, business_city, business_state, business_zip, mail_street_1, mail_street_2, mail_city, mail_state, mail_zip, business_phone, state_of_incorporation) values (NULL ". ", ?" x 18 .")") || die "$!";
+		my $sth2 = $db->prepare_cached("insert into filers (filer_id, filing_id, cik, irs_number, conformed_name, fiscal_year_end, sic_code, business_street_1, business_street_2, business_city, business_state, business_zip, mail_street_1, mail_street_2, mail_city, mail_state, mail_zip, business_phone, state_of_incorporation, year) values (NULL ". ", ?" x 19 .")") || die "$!";
 
-		foreach my $key ('filing_id', 'cik', 'irs_number', 'conformed_name', 'fiscal_year_end', 'sic_code', 'business_street_1', 'business_street_2', 'business_city', 'business_state', 'business_zip', 'mail_street_1', 'mail_street_2', 'mail_city', 'mail_state', 'mail_zip', 'business_phone', 'state_of_incorporation') {
+		foreach my $key ('filing_id', 'cik', 'irs_number', 'conformed_name', 'fiscal_year_end', 'sic_code', 'business_street_1', 'business_street_2', 'business_city', 'business_state', 'business_zip', 'mail_street_1', 'mail_street_2', 'mail_city', 'mail_state', 'mail_zip', 'business_phone', 'state_of_incorporation', 'year') {
 			push (@results, $values->{$key}) ;
 		}
 		$sth2->execute(@results);
