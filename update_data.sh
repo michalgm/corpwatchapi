7ec9dc935fa47e5f66f66aa401f94735de42fe6d
Greg Michalec <greg@primate.net>
2009-10-27 15:57:34 +0000

    added stage to launch publish_data, triggered by presence of first argument
    git-svn-id: file:///var/lib/svn/edgarapi@319 a230b33d-3e21-4b13-a9c7-ec5718a1d03f

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+uploaddata=$1;
 time perl cleanup_state.pl
 time perl fetch_10ks.pl
 time perl fetch_filer_headers.pl
@@ -9,4 +9,7 @@ time perl relationship_wrapper.pl;
 time perl clean_relationships.pl;
 time perl populate_companies.pl;
 time perl export_database_info.pl;
-
+if [ "$uploaddata" == 1 ]; then
+	cd ../utils
+	bash ./publish_data.sh;
+fi;
