2a1c33e56f1b51a941104b887b60f1361c447292
Greg Michalec <greg@primate.net>
2018-06-11 21:57:26 -0700

    remove publish step from update_data

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-uploaddata=$1;
 time perl cleanup_state.pl
 time perl fetch_10ks.pl
 time perl fetch_filer_headers.pl
@@ -8,8 +7,3 @@ time perl update_cik_name_lookup.pl
 time perl relationship_wrapper.pl;
 time perl clean_relationships.pl;
 time perl populate_companies.pl;
-time perl export_database_info.pl;
-if [ "$uploaddata" == 1 ]; then
-	cd ../utils
-	bash ./publish_data.sh;
-fi;
